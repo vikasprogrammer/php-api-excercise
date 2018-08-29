e1dec6bf3c58235aa41aabfbbd7aecfe107fb140
Vikas Singhal <vikas.programmer@gmail.com>
2018-08-29 19:08:52 +0530

    composer update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,7 +81,6 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-$app->register(BC\LumenSwagger\ServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
